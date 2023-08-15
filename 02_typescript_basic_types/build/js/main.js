4bde69345b4175d192639ce6703aab629957c284
aitizaz <atz.softprgmr@gmail.com>
2023-08-15 14:59:57 +0500

    added lessons

---
+++ 02_typescript_basic_types/build/js/main.js
@@ -0,0 +1,20 @@
+"use strict";
+// STRING
+let myName = "Dave";
+// NUMBER
+let meaningOfLife;
+// BOOLEAN
+let isLoading;
+// ANY
+let album;
+// UNION 
+let car;
+let postID;
+let isActive;
+myName = 'john';
+meaningOfLife = 34;
+isLoading = true;
+// FUNCTION PARAMS
+const sum = (a, b) => {
+    return a + b;
+};
---
+++ 02_typescript_basic_types/src/main.ts
@@ -0,0 +1,32 @@
+// STRING
+let myName: string = "Dave"
+
+// NUMBER
+let meaningOfLife: number;
+
+// BOOLEAN
+let isLoading: boolean;
+
+// ANY
+let album: any;
+
+// UNION 
+let car: string | number;
+let postID: string | number;
+let isActive : number | boolean;
+
+myName='john'
+meaningOfLife=34
+isLoading=true
+
+
+
+
+
+// FUNCTION PARAMS
+const sum = (a: number, b:number) => {
+    return a + b;
+}
+
+
+
